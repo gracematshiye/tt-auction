27f26ca93436e24068575c2b11bc658f63dc333c
Felix Moalusi <felixsean@ymail.com>
2016-08-25 12:40:24 +0200

    project layout, and styles

--- src/main/java/com/thoughtworks/ttauction/controller/CarController.java
+++ src/main/java/com/thoughtworks/ttauction/controller/CarController.java
@@ -2,6 +2,7 @@ package com.thoughtworks.ttauction.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -13,5 +14,10 @@ public class CarController {
 
         return new ModelAndView("testJSP", "message", message);
 //        return "testJSP";
+    }
+
+    @RequestMapping(value = "/all-cars", method = RequestMethod.GET)
+    public String displayAllCars(){
+        return "all-cars";
     }
 }
