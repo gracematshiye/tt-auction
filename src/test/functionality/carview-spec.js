c7e7a756ca0675f4b6affdeb07224e6fd7ed8d5b
Felix Moalusi <felixsean@ymail.com>
2016-09-05 22:02:22 +0200

    Testing multiple protractor specs

---
+++ src/test/functionality/carview-spec.js
@@ -0,0 +1,26 @@
+describe('Car view navigation', function() {
+  it('should navigate to car view, and select an image to view', function() {
+  	browser.ignoreSynchronization = true;
+
+  	//browser.get('http://localhost:8080/cars');
+  	browser.get('https://tt-auction.herokuapp.com/cars');
+
+    //Click on a Car
+    var el =  element(by.css('.car'));
+    expect(el.isDisplayed()).toBe(true);
+    el.click();
+    //expect(browser.getCurrentUrl()).toEqual("http://localhost:8080/cars/1");
+    expect(browser.getCurrentUrl()).toEqual("https://tt-auction.herokuapp.com/cars/1");
+
+
+    //Click on a thumbnail
+    var thumb =  element.all(by.css('.imgPreview')).get(1);//select second thumbnail
+    expect(thumb.isDisplayed()).toBe(true);
+    thumb.click();
+
+    var displayedPic = element(by.id('selectedCar'));
+    expect(displayedPic.src == thumb.src);//double check the syntax
+    //.getAttribute('src')
+
+  });
+});
--- src/test/functionality/conf.js
+++ src/test/functionality/conf.js
@@ -1,5 +1,5 @@
 exports.config = {
-  specs: ['mainview-spec.js']
+  specs: ['*-spec.js']
 };
 if (process.env.SNAP_CI) {
   exports.config.chromeDriver = '/usr/local/bin/chromedriver';
