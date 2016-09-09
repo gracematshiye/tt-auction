c72af3316c5622f5a3b44aacaeed70df0ef9969c
Grace <gracem@thoughtworks.com>
2016-09-09 10:57:09 +0200

    add more functional tests for register page

--- src/test/functionality/registerView-spec.js
+++ src/test/functionality/registerView-spec.js
@@ -1,42 +1,42 @@
-//describe('Register page', function() {
-//  it('should display register page', function() {
-//  	browser.ignoreSynchronization = true;
-//
-//  	browser.get('http://localhost:8080');
-//  	//browser.get('https://tt-auction.herokuapp.com');
-//
-//    //Click on a Car
-//    var el =  element(by.css('.car'));
-//    expect(el.isDisplayed()).toBe(true);
-//    el.click();
-//    expect(browser.getCurrentUrl()).toEqual("http://localhost:8080/cars/1");
-////    expect(browser.getCurrentUrl()).toEqual("https://tt-auction.herokuapp.com/cars/1");
-//
-//    //Click bid button
-//    var bidButton = element(by.css('.btnBid'));
-//    expect(bidButton.isDisplayed()).toBe(true);
-//    bidButton.click();
-//    expect(browser.getCurrentUrl()).toEqual("http://localhost:8080/customer/register");
-//
-//    //Fill in all the text box
-//    element(by.model('firstName')).sendKeys('first');
-//    element(by.model('lastName')).sendKeys('lastUser');
-//    element(by.model('username')).sendKeys('firstUser');
-//    element(by.model('password')).sendKeys('user1');
-//    element(by.model('password2')).sendKeys('user1');
-//    element(by.model('dateOfBirth')).sendKeys('10 May 1991');
-//    element(by.model('email')).sendKeys('gracem@ymail.com');
-//    element(by.model('contact')).sendKeys('0783456789');
-//    element(by.model('address')).sendKeys('22 Melle Street');
-//
+describe('Register page', function() {
+  it('should display register page', function() {
+  	browser.ignoreSynchronization = true;
 
-//     var register =  element(by.css('.btnBid'));
-//     expect(register.isDisplayed()).toBe(true);
-//     register.click();
+  	browser.get('http://localhost:8080');
+  	//browser.get('https://tt-auction.herokuapp.com');
+
+    //Click on a Car
+    var el =  element(by.css('.car'));
+    expect(el.isDisplayed()).toBe(true);
+    el.click();
+    expect(browser.getCurrentUrl()).toEqual("http://localhost:8080/cars/1");
+//    expect(browser.getCurrentUrl()).toEqual("https://tt-auction.herokuapp.com/cars/1");
+
+    //Click bid button
+    var bidButton = element(by.css('.btnBid'));
+    expect(bidButton.isDisplayed()).toBe(true);
+    bidButton.click();
+    expect(browser.getCurrentUrl()).toEqual("http://localhost:8080/add-user-bid?carId=1");
+
+//    //Fill in all the text box
+    element(by.model('firstName')).sendKeys('first');
+    element(by.model('lastName')).sendKeys('lastUser');
+    element(by.model('username')).sendKeys('firstUser');
+    element(by.model('password')).sendKeys('user1');
+    element(by.model('password2')).sendKeys('user1');
+    element(by.model('dateOfBirth')).sendKeys('10 May 1991');
+    element(by.model('email')).sendKeys('gracem@ymail.com');
+    element(by.model('contact')).sendKeys('0783456789');
+    element(by.model('address')).sendKeys('22 Melle Street');
+
+
+     var register =  element(by.css('.btnBid'));
+     expect(register.isDisplayed()).toBe(true);
+     register.click();
 //
 //     expect(browser.getCurrentUrl()).toEqual("http://localhost:8080/customer/register/add");
-////     expect(browser.getCurrentUrl()).toEqual("https://tt-auction.herokuapp.com/customer/register/add");
+//     expect(browser.getCurrentUrl()).toEqual("https://tt-auction.herokuapp.com/customer/register/add");
 
-//    });
-//});
+    });
+});
 
