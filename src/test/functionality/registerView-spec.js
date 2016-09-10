97be1f7555f8c37bd5878ba09883b389fe9d5a08
Grace <gracem@thoughtworks.com>
2016-09-10 20:07:59 +0200

    add functional tests of register page for validation

--- src/test/functionality/registerView-spec.js
+++ src/test/functionality/registerView-spec.js
@@ -19,6 +19,56 @@ describe('Register page', function() {
 //    expect(browser.getCurrentUrl()).toEqual("http://localhost:8080/customer/register/carId=1");
     expect(browser.getCurrentUrl()).toEqual("https://tt-auction.herokuapp.com/customer/register/carId=1");
 
+//    /* Test Validation */
+//    // Password Validation
+//    element(by.id('firstName')).sendKeys('first');
+//    element(by.id('lastName')).sendKeys('lastUser');
+//    element(by.id('username')).sendKeys('firstUser');
+//    element(by.id('password')).sendKeys('user12');
+//    element(by.id('password2')).sendKeys('user1');
+//    element(by.id('dateOfBirth')).sendKeys('10 May 1991');
+//    element(by.id('email')).sendKeys('gracem@ymail.com');
+//    element(by.id('contact')).sendKeys('0783456789');
+//    element(by.id('address')).sendKeys('22 Melle Street');
+//
+//     var passwordError = element(by.id('passErr'));
+//     expect(passwordError.getText()).toEqual('Passwords do not match!');
+//    expect(browser.getCurrentUrl()).toEqual("http://localhost:8080/customer/register/carId=1");
+////     expect(browser.getCurrentUrl()).toEqual("https://tt-auction.herokuapp.com/customer/register/carId=1");
+//
+//    // Email Validation
+//    element(by.id('firstName')).sendKeys('first');
+//    element(by.id('lastName')).sendKeys('lastUser');
+//    element(by.id('username')).sendKeys('firstUser');
+//    element(by.id('password')).sendKeys('user1');
+//    element(by.id('password2')).sendKeys('user1');
+//    element(by.id('dateOfBirth')).sendKeys('10 May 1991');
+//    element(by.id('email')).sendKeys('gracem4ymail.com');
+//    element(by.id('contact')).sendKeys('0783456789');
+//    element(by.id('address')).sendKeys('22 Melle Street');
+//
+//     var emailError = element(by.id('emlErr'));
+//     expect(emailError.getText()).toEqual("Incorrect email format");
+//    expect(browser.getCurrentUrl()).toEqual("http://localhost:8080/customer/register/carId=1");
+////     expect(browser.getCurrentUrl()).toEqual("https://tt-auction.herokuapp.com/customer/register/carId=1");
+//
+// // Cellphone number Validation
+//    element(by.id('firstName')).sendKeys('first');
+//    element(by.id('lastName')).sendKeys('lastUser');
+//    element(by.id('username')).sendKeys('firstUser');
+//    element(by.id('password')).sendKeys('user1');
+//    element(by.id('password2')).sendKeys('user1');
+//    element(by.id('dateOfBirth')).sendKeys('10 May 1991');
+//    element(by.id('email')).sendKeys('gracem@ymail.com');
+//    element(by.id('contact')).sendKeys('07345789');
+//    element(by.id('address')).sendKeys('22 Melle Street');
+//
+////     element(by.css('.btnBid')).click();
+//     var contactError = element(by.id('cntErr'));
+//     expect(contactError.getText()).toEqual("Incorrect cell phone number");
+//    expect(browser.getCurrentUrl()).toEqual("http://localhost:8080/customer/register/carId=1");
+////     expect(browser.getCurrentUrl()).toEqual("https://tt-auction.herokuapp.com/customer/register/carId=1");
+
 //    //Fill in all the text box
     element(by.id('firstName')).sendKeys('first');
     element(by.id('lastName')).sendKeys('lastUser');
