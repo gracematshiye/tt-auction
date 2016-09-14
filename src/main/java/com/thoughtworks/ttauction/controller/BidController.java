9cfe4f95a58b93a981db7ca47deee2d0f2736d4a
Felix Moalusi <felixsean@ymail.com>
2016-09-14 11:12:31 +0200

    Integrating changes

--- src/main/java/com/thoughtworks/ttauction/controller/BidController.java
+++ src/main/java/com/thoughtworks/ttauction/controller/BidController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Controller
@@ -27,7 +28,6 @@ public class BidController {
         bid.setStatus(true);
 
         bidService.addBid(bid);
-
         return "redirect:/";
     }
 }
